6de31e412b5e37d210536d2e562f627b1700fa60
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-03-03 12:16:18 +0100

    Add camera follow strategies; Add some more debug information;

--- dist/src/core/Input.js
+++ dist/src/core/Input.js
@@ -3,8 +3,11 @@ export class Input {
     static mouse = { x: 0, y: 0, dx: 0, dy: 0, right: false };
     static debug = false;
     static init() {
-        window.addEventListener("keydown", e => { this.keys.set(e.code, true); if (this.debug)
-            console.log("%c[Input] Key: " + e.code, "color: #ff0"); });
+        window.addEventListener("keydown", e => {
+            this.keys.set(e.code, true);
+            if (this.debug)
+                console.log("%c[Input] Key Down: " + e.code, "color: #ff0");
+        });
         window.addEventListener("keyup", e => this.keys.set(e.code, false));
         window.addEventListener("mousedown", e => { if (e.button === 2)
             this.mouse.right = true; });
@@ -13,11 +16,15 @@ export class Input {
         window.addEventListener("mousemove", e => { this.mouse.dx = e.movementX; this.mouse.dy = e.movementY; });
         window.addEventListener("contextmenu", e => e.preventDefault());
     }
+    // DIE VERMISSTE METHODE
+    static isPressed(code) {
+        return this.keys.get(code) === true;
+    }
     static getAxis(neg, pos) {
         let v = 0;
-        if (this.keys.get(neg))
+        if (this.isPressed(neg))
             v -= 1;
-        if (this.keys.get(pos))
+        if (this.isPressed(pos))
             v += 1;
         return v;
     }
