ce52044609d4d3d5a4b20b28fa8c862b6eac1a45
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-03-05 10:48:26 +0100

    Code style;

---
+++ dist/src/constants/Keys.js
@@ -0,0 +1,19 @@
+export var Keys;
+(function (Keys) {
+    // Bewegung
+    Keys["W"] = "KeyW";
+    Keys["A"] = "KeyA";
+    Keys["S"] = "KeyS";
+    Keys["D"] = "KeyD";
+    Keys["UP"] = "ArrowUp";
+    Keys["DOWN"] = "ArrowDown";
+    Keys["LEFT"] = "ArrowLeft";
+    Keys["RIGHT"] = "ArrowRight";
+    Keys["SPACE"] = "Space";
+    // Ziffern (Kamerasteuerung)
+    Keys["D1"] = "Digit1";
+    Keys["D2"] = "Digit2";
+    Keys["D3"] = "Digit3";
+    Keys["D4"] = "Digit4";
+})(Keys || (Keys = {}));
+//# sourceMappingURL=Keys.js.map
