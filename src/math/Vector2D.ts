36f10c8cf7e8d2bb93e1bbb0a5b217e047e925bc
Stefan Rottensteiner <rottensteiner.stefan@gmx.de>
2026-03-05 10:01:06 +0100

    Enrich Vector2D methods with essential methods for math, collision detection etc.

--- src/math/Vector2D.ts
+++ src/math/Vector2D.ts
@@ -1,29 +1,50 @@
 export class Vector2D {
-  constructor(
-    public x: number = 0,
-    public y: number = 0,
-  ) {}
+  constructor(public x: number = 0, public y: number = 0) {}
 
-  public add(v: Vector2D): Vector2D {
-    this.x += v.x;
-    this.y += v.y;
+  public set(x: number, y: number): this {
+    this.x = x; this.y = y;
     return this;
   }
 
-  public multiply(v: Vector2D): Vector2D {
-    this.x *= v.x;
-    this.y *= v.y;
+  public add(v: Vector2D): this {
+    this.x += v.x; this.y += v.y;
     return this;
   }
 
-  public scale(s: number): Vector2D {
-    this.x *= s;
-    this.y *= s;
+  public sub(v: Vector2D): this {
+    this.x -= v.x; this.y -= v.y;
     return this;
+  }
+
+  public scale(s: number): this {
+    this.x *= s; this.y *= s;
+    return this;
+  }
+
+  public dot(v: Vector2D): number {
+    return this.x * v.x + this.y * v.y;
+  }
+
+  public lengthSq(): number {
+    return this.x * this.x + this.y * this.y;
   }
 
   public length(): number {
-    return Math.sqrt(this.x * this.x + this.y * this.y);
+    return Math.sqrt(this.lengthSq());
+  }
+
+  /**
+   * Berechnet das Quadrat der Distanz zwischen zwei 2D-Punkten.
+   * Ideal für schnelle 2D-Kollisions- oder UI-Distanzchecks.
+   */
+  public distanceToSq(v: Vector2D): number {
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return dx * dx + dy * dy;
+  }
+
+  public distanceTo(v: Vector2D): number {
+    return Math.sqrt(this.distanceToSq(v));
   }
 
   public clone(): Vector2D {
