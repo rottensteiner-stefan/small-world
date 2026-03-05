2bad7deadbe2d20abbbea8898a069972569df9aa
Stefan Rottensteiner <rottensteiner.stefan@gmx.de>
2026-03-05 10:48:26 +0100

    Code style;

---
+++ dist/src/interfaces/IBoundingVolume.js
@@ -0,0 +1,6 @@
+export var BoundingType;
+(function (BoundingType) {
+    BoundingType[BoundingType["SPHERE"] = 0] = "SPHERE";
+    BoundingType[BoundingType["BOX"] = 1] = "BOX";
+})(BoundingType || (BoundingType = {}));
+//# sourceMappingURL=IBoundingVolume.js.map
