c79fcf4fbe37df8ee130e9b9e966a588f13bfb3f
Stefan Rottensteiner <rottensteiner.stefan@gmx.de>
2026-03-06 22:34:47 +0100

    Dynamic version display; Re-introduce /dist

---
+++ dist/src/interfaces/IRenderer.js
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=IRenderer.js.map
---
+++ dist/src/interfaces/RendererTypes.js
@@ -0,0 +1,8 @@
+export var RendererType;
+(function (RendererType) {
+    RendererType["WEBGL1"] = "WEBGL1";
+    RendererType["WEBGL2"] = "WEBGL2";
+    RendererType["WEBGPU"] = "WEBGPU";
+    RendererType["BEST"] = "BEST";
+})(RendererType || (RendererType = {}));
+//# sourceMappingURL=RendererTypes.js.map
