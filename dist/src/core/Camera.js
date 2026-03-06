de2bb7c6cef4499f0ae0fae8e5ca321fdb07fe79
Stefan Rottensteiner <rottensteiner.stefan@gmx.de>
2026-03-06 20:27:12 +0100

    Fix of the day

---
+++ dist/src/core/Camera.js
@@ -0,0 +1,55 @@
+import { Matrix4 } from "../math/Matrix4.js";
+import { Vector3D } from "../math/Vector3D.js";
+export var CameraStrategy;
+(function (CameraStrategy) {
+    CameraStrategy[CameraStrategy["FIXED"] = 0] = "FIXED";
+    CameraStrategy[CameraStrategy["STIFF"] = 1] = "STIFF";
+    CameraStrategy[CameraStrategy["SMOOTH"] = 2] = "SMOOTH";
+})(CameraStrategy || (CameraStrategy = {}));
+export class Camera {
+    projection;
+    position = new Vector3D(0, 10, 20);
+    target = new Vector3D(0, 0, 0);
+    up = new Vector3D(0, 1, 0);
+    strategy = CameraStrategy.SMOOTH;
+    theta = 0;
+    phi = 0.6;
+    radius = 20;
+    lerpFactor = 0.1;
+    constructor(projection) {
+        this.projection = projection;
+    }
+    update(playerPos, dx, dy) {
+        if (dx !== 0 || dy !== 0) {
+            this.theta -= dx * 0.01;
+            this.phi += dy * 0.01;
+            const limit = Math.PI / 2 - 0.01;
+            if (this.phi > limit)
+                this.phi = limit;
+            if (this.phi < -limit)
+                this.phi = -limit;
+        }
+        if (this.strategy !== CameraStrategy.FIXED) {
+            this.target.x = playerPos.x;
+            this.target.y = playerPos.y;
+            this.target.z = playerPos.z;
+        }
+        const idealX = this.target.x + this.radius * Math.sin(this.theta) * Math.cos(this.phi);
+        const idealY = this.target.y + this.radius * Math.sin(this.phi);
+        const idealZ = this.target.z + this.radius * Math.cos(this.theta) * Math.cos(this.phi);
+        if (this.strategy === CameraStrategy.STIFF) {
+            this.position.x = idealX;
+            this.position.y = idealY;
+            this.position.z = idealZ;
+        }
+        else if (this.strategy === CameraStrategy.SMOOTH) {
+            this.position.x += (idealX - this.position.x) * this.lerpFactor;
+            this.position.y += (idealY - this.position.y) * this.lerpFactor;
+            this.position.z += (idealZ - this.position.z) * this.lerpFactor;
+        }
+    }
+    getViewProjection(v, out) {
+        Matrix4.multiply(this.projection.getMatrix(), v, out);
+    }
+}
+//# sourceMappingURL=Camera.js.map
