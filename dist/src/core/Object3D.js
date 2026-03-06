5763171ee02dc340e0a064039896fbae175cf4ee
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-03-06 22:34:36 +0100

    Dynamic version display; Re-introduce /dist

---
+++ dist/src/core/Object3D.js
@@ -0,0 +1,48 @@
+import { Vector3D } from "../math/Vector3D.js";
+import { Matrix4 } from "../math/Matrix4.js";
+import { Color } from "./Color.js";
+export class Object3D {
+    uuid = crypto.randomUUID();
+    name = "";
+    // Rendering Daten
+    geometry = null;
+    bounds = null;
+    color = Color.WHITE;
+    position = new Vector3D(0, 0, 0);
+    rotation = new Vector3D(0, 0, 0);
+    scale = new Vector3D(1, 1, 1);
+    localMatrix = new Matrix4();
+    worldMatrix = new Matrix4();
+    parent = null;
+    children = [];
+    constructor(name = "") {
+        this.name = name;
+    }
+    add(child) {
+        if (child.parent) {
+            child.parent.remove(child);
+        }
+        child.parent = this;
+        this.children.push(child);
+    }
+    remove(child) {
+        const index = this.children.indexOf(child);
+        if (index !== -1) {
+            child.parent = null;
+            this.children.splice(index, 1);
+        }
+    }
+    updateMatrixWorld(force = false) {
+        this.localMatrix.compose(this.position, this.rotation, this.scale);
+        if (this.parent === null) {
+            this.worldMatrix.data.set(this.localMatrix.data);
+        }
+        else {
+            Matrix4.multiply(this.parent.worldMatrix, this.localMatrix, this.worldMatrix);
+        }
+        for (const child of this.children) {
+            child.updateMatrixWorld(force);
+        }
+    }
+}
+//# sourceMappingURL=Object3D.js.map
