2b7e06c3259b2b99776c83dcb9b6fcfc403ca871
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-03-08 19:56:53 +0100

    Clean up

---
+++ dist/src/enums/RendererType.js
@@ -0,0 +1,8 @@
+export var RendererType;
+(function (RendererType) {
+    RendererType["BEST"] = "BEST";
+    RendererType["WEB_GPU"] = "WEB_GPU";
+    RendererType["WEB_GL"] = "WEB_GL";
+    RendererType["CANVAS"] = "CANVAS";
+})(RendererType || (RendererType = {}));
+//# sourceMappingURL=RendererType.js.map
