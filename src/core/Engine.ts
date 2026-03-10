ce82c8337d68faf3c4e796357b91db8ee7a9ff06
Stefan Rottensteiner <rottensteiner.stefan@gmx.de>
2026-03-10 13:37:27 +0100

    Implement basic texture and assessment management stuff

--- src/core/Engine.ts
+++ src/core/Engine.ts
@@ -2,5 +2,5 @@
 import { RendererType } from '../enums/RendererType.js';
 
 export { RendererType }; // <--- WICHTIG: Re-Export hinzufügen
-export const ENGINE_VERSION = "0.8.52";
+export const ENGINE_VERSION = "0.8.53";
 export const DEFAULT_RENDERER = RendererType.BEST;
