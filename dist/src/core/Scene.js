5de61443a31fb2703911b605e656cc8225b70d67
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-03-10 21:55:55 +0100

    Implement SkyBox

---
+++ dist/src/core/Scene.js
@@ -0,0 +1,18 @@
+export class Scene {
+    objects = [];
+    add(obj) {
+        this.objects.push(obj);
+    }
+    remove(obj) {
+        const index = this.objects.indexOf(obj);
+        if (index !== -1)
+            this.objects.splice(index, 1);
+    }
+    update() {
+        for (const obj of this.objects) {
+            if (obj.updateMatrixWorld)
+                obj.updateMatrixWorld(true);
+        }
+    }
+}
+//# sourceMappingURL=Scene.js.map
