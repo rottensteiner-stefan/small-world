e293cc3b2e9fa827e57409ef9629dd19e236d507
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-03-10 13:12:58 +0100

    Add new geometries (pyramid, torus and cylinder).

---
+++ dist/src/geometry/Cylinder.js
@@ -0,0 +1,56 @@
+import { ObjectGeometry } from "./ObjectGeometry.js";
+export class Cylinder extends ObjectGeometry {
+    radius;
+    height;
+    segments;
+    constructor(radius = 1, height = 2, segments = 16) {
+        super();
+        this.radius = radius;
+        this.height = height;
+        this.segments = segments;
+        this.generateGeometryData();
+    }
+    generateGeometryData() {
+        const v = [];
+        const idx = [];
+        const hh = this.height / 2;
+        // --- Seitenwand ---
+        for (let y = 0; y <= 1; y++) {
+            const yPos = y === 0 ? -hh : hh;
+            for (let x = 0; x <= this.segments; x++) {
+                const theta = (x / this.segments) * Math.PI * 2;
+                v.push(this.radius * Math.sin(theta), yPos, this.radius * Math.cos(theta));
+            }
+        }
+        for (let x = 0; x < this.segments; x++) {
+            const first = x;
+            const second = first + this.segments + 1;
+            idx.push(first, second, first + 1);
+            idx.push(second, second + 1, first + 1);
+        }
+        // --- Deckel Oben ---
+        let offset = v.length / 3;
+        v.push(0, hh, 0); // Zentrum Oben
+        for (let x = 0; x <= this.segments; x++) {
+            const theta = (x / this.segments) * Math.PI * 2;
+            v.push(this.radius * Math.sin(theta), hh, this.radius * Math.cos(theta));
+        }
+        for (let x = 0; x < this.segments; x++) {
+            idx.push(offset, offset + x + 1, offset + x + 2);
+        }
+        // --- Deckel Unten ---
+        offset = v.length / 3;
+        v.push(0, -hh, 0); // Zentrum Unten
+        for (let x = 0; x <= this.segments; x++) {
+            const theta = (x / this.segments) * Math.PI * 2;
+            v.push(this.radius * Math.sin(theta), -hh, this.radius * Math.cos(theta));
+        }
+        for (let x = 0; x < this.segments; x++) {
+            idx.push(offset, offset + x + 2, offset + x + 1); // Winding umdrehen für Sichtbarkeit!
+        }
+        this.vertices = new Float32Array(v);
+        this.indices = new Uint16Array(idx);
+        this.computeNormals();
+    }
+}
+//# sourceMappingURL=Cylinder.js.map
