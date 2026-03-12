b9bfc45086b9d688ea4b728c8a4ced6acac41888
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-03-12 11:29:15 +0100

    Bug fixing due to the last refactorings

--- src/core/Engine.ts
+++ src/core/Engine.ts
@@ -2,5 +2,5 @@
 import { RendererType } from '../enums/RendererType.js';
 
 export { RendererType }; // <--- WICHTIG: Re-Export hinzufügen
-export const ENGINE_VERSION = "0.9.3";
+export const ENGINE_VERSION = "0.9.4";
 export const DEFAULT_RENDERER = RendererType.BEST;
