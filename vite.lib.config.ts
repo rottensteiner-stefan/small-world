5fd858a8f34b041016859fb1dfb6ce6b6e6a5739
Stefan Rottensteiner <rottensteiner.stefan@gmx.de>
2026-03-13 17:09:30 +0100

    Crush the code

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,38 +1,38 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-    build: {
-        outDir: "dist",
-        emptyOutDir: false,
-        minify: "terser", // <-- Wechsel auf den aggressiveren Terser
-        target: "esnext",
-        lib: {
-            entry: "src/index.ts",
-            name: "SmallWorld",
-            fileName: () => "small-world.js",
-            formats: ["es"],
-        },
-        terserOptions: {
-            format: {
-                comments: false, // Wirft wirklich ALLE Kommentare restlos raus
-            },
-            compress: {
-                drop_console: true,
-                drop_debugger: true,
-                passes: 2, // Jagt den Optimierer zweimal über den Code (für maximales Quetschen)
-            },
-            mangle: true, // Kürzt alle internen Variablennamen
-        },
-        rollupOptions: {
-            output: {
-                compact: true,
-            },
-        },
+  build: {
+    outDir: "dist",
+    emptyOutDir: false,
+    minify: "terser", // <-- Wechsel auf den aggressiveren Terser
+    target: "esnext",
+    lib: {
+      entry: "src/index.ts",
+      name: "SmallWorld",
+      fileName: () => "small-world.js",
+      formats: ["es"],
     },
-    plugins: [
-        dts({
-            rollupTypes: true,
-        }),
-    ],
+    terserOptions: {
+      format: {
+        comments: false, // Wirft wirklich ALLE Kommentare restlos raus
+      },
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 2, // Jagt den Optimierer zweimal über den Code (für maximales Quetschen)
+      },
+      mangle: true, // Kürzt alle internen Variablennamen
+    },
+    rollupOptions: {
+      output: {
+        compact: true,
+      },
+    },
+  },
+  plugins: [
+    dts({
+      rollupTypes: true,
+    }),
+  ],
 });
