876f312e36bc8b21d48f3f4f5d772ef6e1454142
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-03-24 23:48:38 +0100

    AI-based rework

--- src/core/lights/DirectionalLight.ts
+++ src/core/lights/DirectionalLight.ts
@@ -4,12 +4,21 @@ import { Color } from "../colors/Color.js";
 import { LightType } from "../../enums/LightType.js";
 import { Vector3D } from "../../math/Vector3D.js";
 
+/**
+ * Directional light that emits light in a specific direction.
+ */
 export class DirectionalLight extends AbstractLight {
-  public override readonly type = LightType.DIRECTIONAL;
+  /** @inheritdoc */
+  public override readonly type: LightType = LightType.DIRECTIONAL;
 
-  public override intensity: number = 1.0;
+  /** The direction of the light. */
   public direction: Vector3D = new Vector3D(0, -1, 0).normalize();
 
+  /**
+   * Creates a new DirectionalLight.
+   * @param color The color of the light.
+   * @param intensity The intensity of the light.
+   */
   constructor(color: Color = Color.WHITE, intensity: number = 1.0) {
     super(color, intensity, "DirectionalLight");
   }
