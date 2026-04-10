fb727aa41f7f96b2901035e32fb0e1b544559d79
Stefan Rottensteiner <rottensteiner.stefan@gmx.de>
2026-04-11 01:16:27 +0200

    Dream big, small world

---
+++ public/resources/shaders/web_gl2/chunks/base_vertex_header.glsl
@@ -0,0 +1,13 @@
+#version 300 es
+in vec3 a_position;
+in vec3 a_normal;
+in vec2 a_uv;
+
+uniform mat4 u_vp;
+uniform mat4 u_model;
+uniform vec2 u_texOffset;
+uniform vec2 u_texRepeat;
+
+out vec3 v_worldPos;
+out vec3 v_normal;
+out vec2 v_uv;
