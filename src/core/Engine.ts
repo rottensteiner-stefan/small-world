f567ee2352579cb7da4cd8cfb21dfb9b20e31156
Stefan Rottensteiner <rottensteiner.stefan@gmx.de>
2026-04-15 21:51:34 +0200

    Deep code maintenance

--- src/core/Engine.ts
+++ src/core/Engine.ts
@@ -2,5 +2,5 @@
 import { RendererType } from '../enums/RendererType.js';
 
 export { RendererType }; // <--- WICHTIG: Re-Export hinzufügen
-export const ENGINE_VERSION = "0.15.04";
+export const ENGINE_VERSION = "0.15.05";
 export const DEFAULT_RENDERER = RendererType.BEST;
