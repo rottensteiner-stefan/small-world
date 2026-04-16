2b82720a9e20d1c60cd18c48cd7f02f12448aa4f
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-04-17 01:10:03 +0200

    Further improvements

--- src/core/events/EventDispatcherImpl.ts
+++ src/core/events/EventDispatcherImpl.ts
@@ -1,12 +1,7 @@
 /// src/core/events/EventDispatcherImpl.ts
 
 import { EventType } from "../../enums/index.js";
-import { Events } from "../../interfaces/Events.js";
-
-/**
- * Type definition for event handler functions.
- */
-export type EventHandler = (event: Record<string, unknown>) => void;
+import { Events, EventHandler } from "../../interfaces/index.js";
 
 /**
  * Standard implementation of the Events interface.
