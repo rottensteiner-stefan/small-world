2c7189dcb9accc0907491cb3500ee09fdb2b282f
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-04-18 01:33:52 +0200

    Further improvements

---
+++ dist/src/renderers/WebGL2UniformBuffer.d.ts
@@ -0,0 +1,18 @@
+import { Color, Vector3D } from '../math/index.js';
+/**
+ * Helper to manage WebGL2 Uniform Buffer Objects (UBOs) with std140 layout.
+ */
+export declare class WebGL2UniformBuffer {
+    private _gl;
+    private _buffer;
+    private _data;
+    private _bindingPoint;
+    constructor(gl: WebGL2RenderingContext, sizeInBytes: number, bindingPoint: number);
+    setMatrix(offset: number, matrix: Float32Array): void;
+    setVector3(offset: number, vec: Vector3D): void;
+    setColor(offset: number, col: Color): void;
+    setFloat(offset: number, val: number): void;
+    setInt(offset: number, val: number): void;
+    update(): void;
+    bindToProgram(program: WebGLProgram, blockName: string): void;
+}
