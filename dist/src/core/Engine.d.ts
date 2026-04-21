94a73eec78e5b76bb32c28ea8169e87d84f9508b
Stefan Rottensteiner <rottensteiner.stefan@gmx.de>
2026-04-21 10:31:00 +0200

    Going on ...

--- dist/src/core/Engine.d.ts
+++ dist/src/core/Engine.d.ts
@@ -1,4 +1,4 @@
-import { RendererType } from '../enums/RendererType.js';
+import { RendererType } from '../enums/index.js';
 export { RendererType };
-export declare const ENGINE_VERSION = "0.18.01";
+export declare const ENGINE_VERSION = "0.19.00";
 export declare const DEFAULT_RENDERER: "BEST";
