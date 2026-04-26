8ebd2d9535074bb2558b552766ca816a46e1bd03
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-04-26 23:26:34 +0200

    v0.19.06

---
+++ dist/src/interfaces/LightData.d.ts
@@ -0,0 +1,23 @@
+import { Color } from '../core/colors/Color.js';
+import { Vector3D } from '../math/Vector3D.js';
+/**
+ * Interface representing the data for all lights in a scene.
+ */
+export interface LightDataInterface {
+    /** Ambient light color. */
+    aCol: Color;
+    /** Ambient light intensity. */
+    aIntensity: number;
+    /** Directional light direction. */
+    dDir: Vector3D;
+    /** Directional light color. */
+    dCol: Color;
+    /** Directional light intensity. */
+    dIntensity: number;
+    /** List of point lights. */
+    pLights: any[];
+    /** List of spot lights. */
+    sLights: any[];
+    /** List of area lights. */
+    aLights: any[];
+}
