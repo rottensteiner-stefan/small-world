74c444a2b05572b5edbff3b8a6b72a9574c7ba08
Stefan Rottensteiner <srottensteiner@hausgross.de>
2026-06-22 02:38:42 +0200

    Home Improvement

--- dist/src/core/SmallWorld.d.ts
+++ dist/src/core/SmallWorld.d.ts
@@ -2,7 +2,7 @@ import { CameraInterfaceData, EngineOptions } from '../interfaces/index.js';
 import { Renderer } from '../interfaces/Renderer.js';
 import { Scene } from './Scene.js';
 /** The current engine version. */
-export declare const ENGINE_VERSION = "0.31.0";
+export declare const ENGINE_VERSION = "0.32.0";
 /**
  * Base class for applications built with the SmallWorld engine.
  */
