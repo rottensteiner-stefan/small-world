1c4a701b854570c0ae546540af9a88bf66a90d5c
Stefan Rottensteiner <rottensteiner.stefan@gmx.de>
2026-07-02 01:09:27 +0200

    Enhance detailed capability logging in SmallWorld and GadgetInspector

--- src/tools/GadgetInspector.ts
+++ src/tools/GadgetInspector.ts
@@ -104,20 +104,34 @@ export class GadgetInspector {
     ).addFolder({ title: "Capabilities", expanded: false });
 
     const caps = {
-      WebGL1: DeviceCaps.hasFeature(DeviceFeature.WEBGL1),
-      WebGL2: DeviceCaps.hasFeature(DeviceFeature.WEBGL2),
-      WebGPU: DeviceCaps.hasFeature(DeviceFeature.WEBGPU),
-      MaxTexSize: DeviceCaps.getLimit(DeviceLimit.MAX_TEXTURE_SIZE),
-      MaxAnisotropy: DeviceCaps.getLimit(DeviceLimit.MAX_ANISOTROPY),
-      MaxUBOSize: DeviceCaps.getLimit(DeviceLimit.MAX_UNIFORM_BUFFER_SIZE),
+      WebGL1: DeviceCaps.hasFeature(DeviceFeature.WEBGL1) ? "Yes" : "No",
+      WebGL2: DeviceCaps.hasFeature(DeviceFeature.WEBGL2) ? "Yes" : "No",
+      WebGPU: DeviceCaps.hasFeature(DeviceFeature.WEBGPU) ? "Yes" : "No",
+      TexSize: DeviceCaps.getLimit(DeviceLimit.MAX_TEXTURE_SIZE),
+      TexUnits: DeviceCaps.getLimit(DeviceLimit.MAX_TEXTURE_IMAGE_UNITS),
+      Anisotropy: DeviceCaps.getLimit(DeviceLimit.MAX_ANISOTROPY),
+      UBOSize: DeviceCaps.getLimit(DeviceLimit.MAX_UNIFORM_BUFFER_SIZE),
+      MSAA: DeviceCaps.getLimit(DeviceLimit.MAX_MSAA_SAMPLES),
+      VertAttrs: DeviceCaps.getLimit(DeviceLimit.MAX_VERTEX_ATTRIBUTES),
+      VertUnis: DeviceCaps.getLimit(DeviceLimit.MAX_VERTEX_UNIFORM_VECTORS),
+      FragUnis: DeviceCaps.getLimit(DeviceLimit.MAX_FRAGMENT_UNIFORM_VECTORS),
+      FloatTex: DeviceCaps.hasFeature(DeviceFeature.FLOAT_TEXTURES) ? "Yes" : "No",
+      CompTex: DeviceCaps.hasFeature(DeviceFeature.COMPRESSED_TEXTURES) ? "Yes" : "No",
     };
 
     capsFolder.addBinding(caps, "WebGL1", { readonly: true, label: "WebGL1" });
     capsFolder.addBinding(caps, "WebGL2", { readonly: true, label: "WebGL2" });
     capsFolder.addBinding(caps, "WebGPU", { readonly: true, label: "WebGPU" });
-    capsFolder.addBinding(caps, "MaxTexSize", { readonly: true, label: "Max Tex Size" });
-    capsFolder.addBinding(caps, "MaxAnisotropy", { readonly: true, label: "Max Anisotropy" });
-    capsFolder.addBinding(caps, "MaxUBOSize", { readonly: true, label: "Max UBO Size" });
+    capsFolder.addBinding(caps, "TexSize", { readonly: true, label: "Max Tex Size" });
+    capsFolder.addBinding(caps, "TexUnits", { readonly: true, label: "Max Tex Units" });
+    capsFolder.addBinding(caps, "Anisotropy", { readonly: true, label: "Max Anisotropy" });
+    capsFolder.addBinding(caps, "UBOSize", { readonly: true, label: "Max UBO Size" });
+    capsFolder.addBinding(caps, "MSAA", { readonly: true, label: "Max MSAA" });
+    capsFolder.addBinding(caps, "VertAttrs", { readonly: true, label: "Max Vert Attrs" });
+    capsFolder.addBinding(caps, "VertUnis", { readonly: true, label: "Max Vert Unis" });
+    capsFolder.addBinding(caps, "FragUnis", { readonly: true, label: "Max Frag Unis" });
+    capsFolder.addBinding(caps, "FloatTex", { readonly: true, label: "Float Tex" });
+    capsFolder.addBinding(caps, "CompTex", { readonly: true, label: "Compressed Tex" });
 
     // 2. Create Highlight Mesh (Neon Cyan Wireframe)
     const geo = new Cube({ size: 1 });
