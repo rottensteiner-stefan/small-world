e859e70ab491c04ada6ac6f183f33600a477066e
Stefan Rottensteiner <rottensteiner.stefan@gmx.de>
2026-07-17 23:43:49 +0200

    0.62.0

--- src/core/SmallWorld.ts
+++ src/core/SmallWorld.ts
@@ -21,7 +21,7 @@ import { CollisionVisualizer, OctreeVisualizer } from "../utils/index.js";
 import { GadgetInspector } from "../tools/GadgetInspector.js";
 
 /** The current engine version. */
-export const ENGINE_VERSION = "0.61.0";
+export const ENGINE_VERSION = "0.62.0";
 
 /**
  * Base class for applications built with the SmallWorld engine.
